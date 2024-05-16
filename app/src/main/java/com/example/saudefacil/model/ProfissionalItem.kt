34984105eafcbce284982a029e100888abed82a5
Arthur <arthur.de.lima.araujo@gmail.com>
2024-05-16 16:05:41 -0300

    Iniciando Projeto

---
+++ app/src/main/java/com/example/saudefacil/model/ProfissionalItem.kt
@@ -0,0 +1,4 @@
+package com.example.saudefacil.model
+
+class ProfissionalItem {
+}
---
+++ app/src/main/java/com/example/saudefacil/ui/agenda/AgendaActivity.kt
@@ -0,0 +1,21 @@
+package com.example.saudefacil.ui.agenda
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.saudefacil.R
+
+class AgendaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_agenda2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
