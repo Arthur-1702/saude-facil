3a30b3887e9d759fb15293360b3747efef05b1ce
Arthur <arthur.de.lima.araujo@gmail.com>
2024-06-01 15:45:21 -0300

    Home atualizada

--- app/src/main/java/com/example/saudefacil/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/saudefacil/ui/home/HomeActivity.kt
@@ -1,21 +1,45 @@
+// HomeActivity.kt
 package com.example.saudefacil.ui.home
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.saudefacil.R
+import android.widget.LinearLayout
+import com.example.saudefacil.ui.laboratorio.LaboratorioActivity
+import com.example.saudefacil.ui.profissional.ProfissionalActivity
+import com.example.saudefacil.ui.agenda.AgendaActivity
+import com.example.saudefacil.ui.login.LoginActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(com.example.saudefacil.R.layout.activity_home)
+
+        // Encontrando os LinearLayouts
+        val btnLaboratorios: LinearLayout = findViewById(com.example.saudefacil.R.id.btnLaboratorios)
+        val btnProfissionaisSaude: LinearLayout = findViewById(com.example.saudefacil.R.id.btnProfissionaisSaude)
+        val btnAgenda: LinearLayout = findViewById(com.example.saudefacil.R.id.btnAgenda)
+        val loginButton: LinearLayout = findViewById(com.example.saudefacil.R.id.loginButton)
+
+        // Configurando os listeners
+        btnLaboratorios.setOnClickListener {
+            val intent = Intent(this, LaboratorioActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnProfissionaisSaude.setOnClickListener {
+            val intent = Intent(this, ProfissionalActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnAgenda.setOnClickListener {
+            val intent = Intent(this, AgendaActivity::class.java)
+            startActivity(intent)
+        }
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
