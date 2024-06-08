96bf602bc325b5478318ca4b1095e5f4789c8418
thiagohss21 <thiagohss21@hotmail.com>
2024-06-08 18:30:38 -0300

    atualização de arquivos

--- app/src/main/java/com/example/saudefacil/ui/historico/HistoricoActivity.kt
+++ app/src/main/java/com/example/saudefacil/ui/historico/HistoricoActivity.kt
@@ -1,4 +1,4 @@
-package com.example.saudefacil.ui.agenda
+package com.example.saudefacil.ui.historico
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,8 +11,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.saudefacil.R
 import com.example.saudefacil.databinding.ActivityHistoricoBinding
 
+
+data class HistoricoItem(val nome: String, val data: String)
 class HistoricoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHistoricoBinding
@@ -32,8 +35,9 @@ class HistoricoActivity : AppCompatActivity() {
 
         adapter = HistoricoAdapter(historico)
 
-        binding.recyclerView.adapter = adapter
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+        binding.recyclerViewHistorico.adapter = adapter
+        binding.recyclerViewHistorico.layoutManager = LinearLayoutManager(this)
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -45,8 +49,8 @@ class HistoricoActivity : AppCompatActivity() {
     inner class HistoricoAdapter(private val historicoList: List<HistoricoItem>) : RecyclerView.Adapter<HistoricoAdapter.ViewHolder>() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.activity_historico, parent, false)
-            return ViewHolder(itemView)
+            val textView = TextView(parent.context)
+            return ViewHolder(textView)
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -58,14 +62,17 @@ class HistoricoActivity : AppCompatActivity() {
             return historicoList.size
         }
 
-        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            private val textNome: TextView = itemView.findViewById(R.id.textNome)
-            private val textData: TextView = itemView.findViewById(R.id.textData)
+       inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            private val textNome: TextView = itemView.findViewById(com.example.saudefacil.R.id.profissional)
+            private val textData: TextView = itemView.findViewById(com.example.saudefacil.R.id.data)
 
-            fun bind(historicoItem: HistoricoItem) {
-                textNome.text = historicoItem.nome
-                textData.text = historicoItem.data
-            }
+           fun bind(historicoItem: HistoricoItem) {
+               textNome.text = historicoItem.nome
+               textData.text = historicoItem.data
+           }
         }
     }
 }
+
+
+
