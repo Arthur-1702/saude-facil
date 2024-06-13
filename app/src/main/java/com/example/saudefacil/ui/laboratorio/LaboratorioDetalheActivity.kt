d027b86ef8cbf0569d90239876f994eab09361d6
juliasantosslv <julia.santoss004@gmail.com>
2024-06-13 17:00:42 -0300

    logica de laboratorio detalhamento

--- app/src/main/java/com/example/saudefacil/ui/laboratorio/LaboratorioDetalheActivity.kt
+++ app/src/main/java/com/example/saudefacil/ui/laboratorio/LaboratorioDetalheActivity.kt
@@ -6,8 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.saudefacil.R
+import com.example.saudefacil.databinding.ActivityLaboratorioDetalheBinding
 
 class LaboratorioDetalheActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLaboratorioDetalheBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +21,22 @@ class LaboratorioDetalheActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding = ActivityLaboratorioDetalheBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Retrieve the data from the intent
+        val laboratorioName = intent.getStringExtra("laboratorio_name")
+        val laboratorioCep = intent.getStringExtra("laboratorio_cep")
+        val laboratorioAddress = intent.getStringExtra("laboratorio_address")
+        val laboratorioPhone = intent.getStringExtra("laboratorio_phone")
+        val laboratorioFuncionamento = intent.getStringExtra("laboratorio_funcionamento")
+
+        // Encontre os TextViews no layout
+        binding.nomeLab.text = laboratorioName ?: "N/A"
+        binding.cep.text = laboratorioCep ?: "N/A"
+        binding.enderecoLab.text = laboratorioAddress ?: "N/A"
+        binding.telefoneLab.text = laboratorioPhone ?: "N/A"
+        binding.funcionamento.text = laboratorioFuncionamento ?: "N/A"
     }
 }
